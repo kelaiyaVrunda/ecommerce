237763c812f2e0639a1a0cb669442e7da21a756b
kelaiyaVrunda <165145147+kelaiyaVrunda@users.noreply.github.com>
2024-09-27 23:27:37 +0530

    homepage

--- ecomm/app/models.py
+++ ecomm/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY_CHOICES=(
+    ('TX','TEXTILE'),
+    ('PT','PAINTING'),
+    ('PC','POTTERY & CERAMICS'),
+    ('SC','SCULPTURES & CARVINGS'),
+    ('HL','HANDLOOMS'),
+    ('MC','METALCRAFTS'),
+    ('WV','WEAVING'),
+)
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    description=models.TextField()
+    composition=models.TimeField(default='')
+    prodapp=models.TextField(default='')
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_image=models.ImageField(upload_to='product')
+    def __str__(self):
+      return self.title
